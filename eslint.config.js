e89ed79e4111e7626eba116f0a9f0cfd80fdc907
dipak458 <dipakjaiswal811@gmail.com>
2026-05-22 07:49:47 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import firebaseRulesPlugin from '@firebase/eslint-plugin-security-rules';
+
+export default [
+  {
+    ignores: ['dist/**/*']
+  },
+  {
+    files: ['firestore.rules', 'DRAFT_firestore.rules'],
+    plugins: {
+      '@firebase/security-rules': firebaseRulesPlugin
+    },
+    rules: {
+      '@firebase/security-rules/no-unprotected-writes': 'error',
+      '@firebase/security-rules/no-unprotected-reads': 'error'
+    }
+  }
+];
